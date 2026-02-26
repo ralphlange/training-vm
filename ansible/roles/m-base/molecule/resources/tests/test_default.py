dabad186c842278d54efb2c985157e633f607715
Simon Rose <simon.rose@ess.eu>
2026-02-26 17:09:16 +0000

    Add tests for m-base
    Three simple tests:
    * RELEASE.local is created correctly
    * softIoc* are on path
    * EPICS tools are on path

--- ansible/roles/m-base/molecule/resources/tests/test_default.py
+++ ansible/roles/m-base/molecule/resources/tests/test_default.py
@@ -1,2 +1,26 @@
-def test_no_op(host):
-    assert True
+import pytest
+
+EPICS_DEV_USER = "epics-dev"
+RELEASE_LOCAL_PATH = "/opt/epics/RELEASE.local"
+
+
+@pytest.mark.parametrize("target", ["EPICS_BASE", "PVXS"])
+def test_release_dot_local_is_populated(host, target):
+    assert any(
+        line.startswith(f"{target}=")
+        for line in host.file(RELEASE_LOCAL_PATH).content_string.splitlines()
+    )
+
+
+@pytest.mark.parametrize("executable", ["softIoc", "softIocPVA", "softIocPVX"])
+def test_softIocXXX_is_on_path(host, executable):
+    with host.sudo(EPICS_DEV_USER):
+        cmd = host.run(f"echo exit | bash -lc '{executable}'")
+        assert cmd.succeeded
+
+
+@pytest.mark.parametrize("tool", ["caget", "pvget", "pvxget"])
+def test_available_tools(host, tool):
+    with host.sudo(EPICS_DEV_USER):
+        cmd = host.run(f"bash -lc '{tool} -h'")
+        assert cmd.succeeded
