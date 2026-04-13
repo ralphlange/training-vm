ralphlange/training-vm

commit 14d571c76c2a17a62db0bb74fff7aab831886e8e

Move configuration files from configs/ to vars/
- requires explicit configuration from command line
  using '-e "@vars/foobar.yml"'

initial_setup.sh
@@ -14,4 +14,4 @@ fi
 
 cd /ansible
 ansible-galaxy install -r requirements.yml || true
-ansible-playbook -e initial_setup=true $ansible_args playbook.yml
+ansible-playbook $ansible_args playbook.yml -e @local.yml -e initial_setup=true

.github/workflows/Containerfile.debian
@@ -9,6 +9,6 @@ WORKDIR /ansible
 RUN apt-get update && apt-get install -y git ansible ansible-lint
 
 RUN ansible-galaxy install -r requirements.yml || true
-RUN ansible-playbook playbook.yml -e "setup=ci-base-image"
+RUN ansible-playbook playbook.yml -e "@vars/ci-base-image.yml"
 
 RUN rm -fr /ansible

.github/workflows/Containerfile.fedora
@@ -10,6 +10,6 @@ COPY ansible /ansible
 WORKDIR /ansible
 
 RUN ansible-galaxy install -r requirements.yml || true
-RUN ansible-playbook playbook.yml -e "setup=ci-base-image"
+RUN ansible-playbook playbook.yml -e "@vars/ci-base-image.yml"
 
 RUN rm -fr /ansible

.github/workflows/Containerfile.rocky
@@ -10,6 +10,6 @@ COPY ansible /ansible
 WORKDIR /ansible
 
 RUN ansible-galaxy install -r requirements.yml || true
-RUN ansible-playbook playbook.yml -e "setup=ci-base-image"
+RUN ansible-playbook playbook.yml -e "@vars/ci-base-image.yml"
 
 RUN rm -fr /ansible

.github/workflows/Containerfile.ubuntu
@@ -12,6 +12,6 @@ COPY ansible /ansible
 WORKDIR /ansible
 
 RUN ansible-galaxy install -r requirements.yml || true
-RUN ansible-playbook playbook.yml -e "setup=ci-base-image"
+RUN ansible-playbook playbook.yml -e "@vars/ci-base-image.yml"
 
 RUN rm -fr /ansible

.github/workflows/rebuild-container-images.yml
@@ -10,7 +10,7 @@ on:
       - '.github/workflows/Containerfile.*'
       - 'ansible/roles/initial_setup/**'
       - 'ansible/roles/m-base/**'
-      - 'ansible/configs/ci-base-image.yml'
+      - 'ansible/vars/ci-base-image.yml'
       - 'ansible/playbook.yml'
       - 'ansible/requirements.yml'
       - 'initial_setup.sh'
@@ -21,7 +21,7 @@ on:
       - '.github/workflows/Containerfile.*'
       - 'ansible/roles/initial_setup/**'
       - 'ansible/roles/m-base/**'
-      - 'ansible/configs/ci-base-image.yml'
+      - 'ansible/vars/ci-base-image.yml'
       - 'ansible/playbook.yml'
       - 'ansible/requirements.yml'
       - 'initial_setup.sh'

.github/workflows/test-roles-modules.yml
@@ -7,7 +7,7 @@ on:
       - 'parallel-ci-pipelines*'
     paths:
       - '.github/workflows/test-roles-modules.yml'
-      - 'ansible/configs/ci-*.yml'
+      - 'ansible/vars/ci-*.yml'
       - 'ansible/playbook.yml'
       - 'ansible/requirements.yml'
       - 'ansible/roles/m-*/**'
@@ -15,7 +15,7 @@ on:
   pull_request:
     paths:
     - '.github/workflows/test-roles-modules.yml'
-    - 'ansible/configs/ci-*.yml'
+    - 'ansible/vars/ci-*.yml'
     - 'ansible/playbook.yml'
     - 'ansible/requirements.yml'
     - 'ansible/roles/m-*/**'
@@ -60,4 +60,4 @@ jobs:
       - name: Build ${{ matrix.conf.name }}
         run: |
           cd ansible
-          ansible-playbook -v playbook.yml -e "setup=${{ matrix.conf.id }}"
+          ansible-playbook -v playbook.yml -e "@vars/${{ matrix.conf.id }}.yml"

.github/workflows/test-roles-oac-tree.yml
@@ -7,14 +7,14 @@ on:
       - 'parallel-ci-pipelines*'
     paths:
       - '.github/workflows/test-roles-oac-tree.yml'
-      - 'ansible/configs/ci-*.yml'
+      - 'ansible/vars/ci-*.yml'
       - 'ansible/playbook.yml'
       - 'ansible/requirements.yml'
       - 'ansible/roles/oac-tree/**'
   pull_request:
     paths:
     - '.github/workflows/test-roles-oac-tree.yml'
-    - 'ansible/configs/ci-*.yml'
+    - 'ansible/vars/ci-*.yml'
     - 'ansible/playbook.yml'
     - 'ansible/requirements.yml'
     - 'ansible/roles/oac-tree/**'
@@ -58,4 +58,4 @@ jobs:
       - name: Build ${{ matrix.conf.name }}
         run: |
           cd ansible
-          ansible-playbook -v playbook.yml -e "setup=${{ matrix.conf.id }}"
+          ansible-playbook -v playbook.yml -e "@vars/${{ matrix.conf.id }}.yml"

commit f4d0ca0f8277fe048da79d13ad57273f1de7f6b7

fix: use correct configuration directory in initial_setup.sh
- bug was introduced with the changes in 14d571c7

initial_setup.sh
@@ -14,4 +14,4 @@ fi
 
 cd /ansible
 ansible-galaxy install -r requirements.yml || true
-ansible-playbook $ansible_args playbook.yml -e @local.yml -e initial_setup=true
+ansible-playbook $ansible_args playbook.yml -e @vars/local.yml -e initial_setup=true